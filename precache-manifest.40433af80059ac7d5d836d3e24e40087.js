76f01cbeba454b36050d7b66a57be354a2d8e32a
Márcio Medeiros <marciomed07@gmail.com>
2020-09-10 20:32:32 -0300

    Updates

---
+++ precache-manifest.40433af80059ac7d5d836d3e24e40087.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "281c6c6997451e639790a40aabe697e8",
+    "url": "/how-long/index.html"
+  },
+  {
+    "revision": "317589b750266554f0d3",
+    "url": "/how-long/static/css/main.a7adf2c0.chunk.css"
+  },
+  {
+    "revision": "0e6a0c5c5e15a4694ae2",
+    "url": "/how-long/static/js/2.399b5df1.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/how-long/static/js/2.399b5df1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "317589b750266554f0d3",
+    "url": "/how-long/static/js/main.7caed881.chunk.js"
+  },
+  {
+    "revision": "3c7b51b4c19b118c2eec",
+    "url": "/how-long/static/js/runtime-main.e720c2cb.js"
+  }
+]);
