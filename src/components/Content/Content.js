101fe8cb8942a59ccbc09b6055304220c7ce9038
Márcio Medeiros <marciomed07@gmail.com>
2020-09-11 14:33:02 -0300

    Export events as json

--- src/components/Content/Content.js
+++ src/components/Content/Content.js
@@ -2,13 +2,52 @@ import React, { useState } from "react";
 import "./Content.css";
 import EventCard from "../EventCard/EventCard";
 import CardHolder from "../CardHolder/CardHolder";
-import { parseISO } from "date-fns";
+import { formatISO, parseISO } from "date-fns";
 
 let initialEvents = JSON.parse(localStorage.getItem("events") || "[]");
 initialEvents.map((event) => {
 	event.date = parseISO(event.date);
 });
 let events = initialEvents;
+
+function exportData() {
+	const file = new Blob([JSON.stringify(events)], {
+		type: "application/json",
+	});
+	const fileName = "how_long_" + formatISO(new Date(), { format: "basic" });
+	if (window.navigator.msSaveOrOpenBlob) {
+		window.navigator.msSaveOrOpenBlob(file, fileName);
+		return;
+	}
+	const a = document.createElement("a");
+	const url = URL.createObjectURL(file);
+	a.href = url;
+	a.download = fileName;
+	document.body.appendChild(a);
+	a.click();
+	setTimeout(() => {
+		document.body.removeChild(a);
+		window.URL.revokeObjectURL(url);
+	});
+}
+
+function importData() {
+	const input = document.getElementById("import-input");
+	// input.click();
+}
+
+function proccessData(event) {
+	const file = event.target.files[0];
+	console.log(file);
+	const fr = new FileReader();
+	fr.onload = (e) => {
+		console.log(e);
+		const result = JSON.parse(e.target.result);
+		console.log(result);
+	};
+
+	fr.readAsText(file);
+}
 
 function Content(props) {
 	function buildCards(events) {
@@ -40,6 +79,32 @@ function Content(props) {
 	const [cards, setCards] = useState(buildCards(initialEvents));
 	return (
 		<div className="content">
+			<div className="data-buttons">
+				<button
+					onClick={() => {
+						exportData();
+					}}
+				>
+					Export
+				</button>
+				<button
+					onClick={() => {
+						importData();
+					}}
+				>
+					Import
+				</button>
+				<input
+					id="import-input"
+					type="file"
+					accept="application/json"
+					multiple={false}
+					onChange={(event) => {
+						proccessData(event);
+					}}
+					hidden
+				/>
+			</div>
 			<CardHolder>
 				{cards}
 				<EventCard creator={true} addEvent={addEvent} />
